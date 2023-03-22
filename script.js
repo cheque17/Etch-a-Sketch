df70bf6ba4ffbc22c7571b1c424d25c01fd5f5e2
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-21 18:58:25 -0600

    Edit script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const container = document.querySelector('#container');
+
+let items = [];
+for(i=0; i<(16*16); i++){
+    let item = document.createElement('div');
+    items.push(item);
+    container.appendChild(item);
+}
