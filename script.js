1600311cd865b4aed67fd0674952c2d61df08c62
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-23 14:29:09 -0600

    Edit script.js

--- script.js
+++ script.js
@@ -7,31 +7,31 @@ let items = [];
 const promptGenerator = document.querySelector('#promptGenerator')
 
 promptGenerator.addEventListener('click', function (size){
-    size = prompt('Please, enter the size of the grid');
-    if (size>100){
-        alert("Please, try again paying attention to the instructions.");
-    return; } 
-    console.log(size);
-    container.style.display = 'grid';
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    container.style.gap = '0';
-    for(i=0; i<(size*size); i++){
-        let item = document.createElement('div');
-        items.push(item);
-        container.appendChild(item);   
-        }
-    items.forEach( function(item) {
-        item.style.height = `(512/${size})px`;
-        item.style.width = `(512/${size})px`;
-        })
+	size = prompt('Please, enter the size of the grid');
+	if (size>100){
+		alert("Please, try again paying attention to the instructions.");
+	return; } 
+	console.log(size);
+	container.style.display = 'grid';
+	container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	container.style.gap = '0';
+	for(i=0; i<(size*size); i++){
+		let item = document.createElement('div');
+		items.push(item);
+		container.appendChild(item);   
+		}
+	items.forEach( function(item) {
+		item.style.height = `(512/${size})px`;
+		item.style.width = `(512/${size})px`;
+		})
 
-    
-    items.forEach ( (item) => {
-        item.addEventListener('mouseover', function () {
-            item.style.backgroundColor = 'black';
-        })
-        })
+	
+	items.forEach ( (item) => {
+		item.addEventListener('mouseover', function () {
+			item.style.backgroundColor = 'black';
+		})
+	})
 })
 
 /*
