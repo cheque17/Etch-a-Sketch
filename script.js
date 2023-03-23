38cf48810fc5d1ef7ce8cadbf07934720f2cdc85
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-23 01:40:08 -0600

    Edit script.js styles.css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('#container');
 
+
 let size = 0;
 let items = [];
 
@@ -9,29 +10,42 @@ promptGenerator.addEventListener('click', function (size){
     size = prompt('Please, enter the size of the grid');
     if (size>100){
         alert("Please, try again paying attention to the instructions.");
-    } else {    
+    return; } 
+    console.log(size);
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gap = '0';
+    for(i=0; i<(size*size); i++){
+        let item = document.createElement('div');
+        items.push(item);
+        container.appendChild(item);   
+        }
+})
+ 
+
+
+console.log(size);
+/*
     for(i=0; i<(size*size); i++){
     let item = document.createElement('div');
     items.push(item);
     container.appendChild(item);   
     }
-1
+
     items.forEach( function(item) {
     item.style.height = `(512/${size})px`;
     item.style.width = `(512/${size})px`;
-    }
-    )
+    })
 
-    }
-})
-
+    
 items.forEach ( (item) => {
     item.addEventListener('mouseover', function () {
         item.style.backgroundColor = 'black';
     })
 })
 
-/*let items = [];
+let items = [];
 for(i=0; i<(16*16); i++){
     let item = document.createElement('div');
     items.push(item);
